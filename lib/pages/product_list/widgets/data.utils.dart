baac6087e023ace004370187b332685323aa4023
chaerinyeon <167524718+chaerinyeon@users.noreply.github.com>
2024-11-27 01:32:06 +0900

    --

--- lib/pages/product_list/widgets/data.utils.dart
+++
@@ -1,8 +0,0 @@
-import 'package:intl/intl.dart';
-
-class DataUtils {
-  static final oCcy = NumberFormat('#,###', 'ko_KR'); //intl
-  static String calcToWon(int price) {
-    return "${oCcy.format(price)}원";
-  }
-}
